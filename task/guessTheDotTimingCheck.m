1900accf5556469116db910f9c09446cbc24bee0
Flavio Ragni <47398415+flavioragni@users.noreply.github.com>
2019-11-27 16:39:13 +0100

    Update guessTheDotTimingCheck.m

--- task/guessTheDotTimingCheck.m
+++ task/guessTheDotTimingCheck.m
@@ -1,10 +1,9 @@
 clear all
-% pathToRes=fullfile('C:\Users\utente\Dropbox\fMRI Study\Scripts\res');
+
 pathToRes=fullfile('D:\Experiments\Angelika\Flavio\guessTheDot\res');
 iSub=99;
 condition='PERCEPTION';%'IMAGERY'; %'PERCEPTION';
 iRun=4;
-%load(fullfile('C:\Users\flavio.ragni\Dropbox\guessTheDot\Scripts\res\', sprintf('guessTheDot_S%02d_R%02d_%s.mat',iSub, iRun, condition)));
 load(fullfile('D:\Experiments\Angelika\Flavio\guessTheDot\res', sprintf('guessTheDot_S%02d_R%02d_%s.mat',iSub, iRun, condition)));
 for k=1:length(ExpInfo.TrialInfo)
     check(k,1)=ExpInfo.TrialInfo(k).tStart
