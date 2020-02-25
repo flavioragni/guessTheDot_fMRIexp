22cac8b9b7842199f3f0d62dd52d88f13aef5ea1
Flavio Ragni <47398415+flavioragni@users.noreply.github.com>
2020-02-25 10:00:56 +0100

    Update GTD_searchlight_permutations_singleStim_CLUSTER_job_submit.m

--- analyses/MVPA/permutation_testing/Searchlight-based/GTD_searchlight_permutations_singleStim_CLUSTER_job_submit.m
+++ analyses/MVPA/permutation_testing/Searchlight-based/GTD_searchlight_permutations_singleStim_CLUSTER_job_submit.m
@@ -10,7 +10,7 @@ subList=[7,9,12,14,16,17,18,19,20,22,23,24,25,26,27,28,29,30,31,33,34];
 condition={'PERCEPTION','IMAGERY','CROSS'};
 for iCond=1:length(condition)
     for iSub=1:length(subList)
-        job.createTask(@GTD_runpermutations_singleStim_CLUSTER, 0,{subList(iSub), condition{iCond}});
+        job.createTask(@GTD_searchlight_runpermutations_singleStim_CLUSTER, 0,{subList(iSub), condition{iCond}});
     end
 end
 %% Run the analysis
